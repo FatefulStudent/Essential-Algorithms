13600371cd6aedf3b82ed72d86333f6f93b2625b
fateful <vladislavvladislavovich@gmail.com>
2019-09-12 20:23:49 +0300

    Added improved algorithm for finding factors

--- Chapter 2 Numerical/findingFactors.cpp
+++ Chapter 2 Numerical/findingFactors.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// just cycling every integer until our number 
+// and checking whether it is a divider
 vector<int> naiveAlgorithm(int number)
 {
     int divider = 2;
@@ -22,10 +24,49 @@ vector<int> naiveAlgorithm(int number)
     return factors;
 }
 
+// we dont need to check all even numbers after checking 2
+// we need to perform search until sqrt(number)
+// updating maxfactor based on number
+vector<int> improvedAlgorithm(int number)
+{
+    vector<int> factors;
+
+    while (number % 2 == 0)
+    {
+        factors.push_back(2);
+        number /= 2;
+    }
+    int oddDivider = 3;
+    double maxFactor = sqrt((double)number);
+    while (oddDivider <= maxFactor)
+    {
+        while (number % oddDivider == 0)
+        {
+            factors.push_back(oddDivider);
+            number /= oddDivider;
+            maxFactor = sqrt((double)number);
+        }
+        oddDivider += 2;
+    }
+
+    if (number != 1)
+        factors.push_back(number);
+
+    return factors;
+}
+
 int main()
 {
-    
-    auto factors = naiveAlgorithm(126);
+    vector<int> factors;
+    factors = naiveAlgorithm(126);
+
+    for (auto iter = factors.begin(); iter!=factors.end(); iter++)
+    {
+        cout << *iter << " ";
+    }
+    cout << endl;
+
+    factors = improvedAlgorithm(126);
 
     for (auto iter = factors.begin(); iter!=factors.end(); iter++)
     {
