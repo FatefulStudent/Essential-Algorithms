d2ecb6b2398c3d9be4ec7fcacdd73f9732339134
fateful <vladislavvladislavovich@gmail.com>
2019-09-13 23:56:54 +0300

    Started linked list

---
+++ Chapter 3 linked lists/linkedListsOneWay.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+class IntegerCell
+{
+public:
+    int value;
+    IntegerCell * nextCell;
+    IntegerCell();
+    IntegerCell(int);
+    ~IntegerCell();
+};
+
+IntegerCell::IntegerCell()
+{
+    value = 0;
+    nextCell = 0;
+}
+
+IntegerCell::IntegerCell(int a)
+{
+    value = a;
+    nextCell = 0;
+}
+
+IntegerCell::~IntegerCell()
+{
+    free(nextCell);
+}
+
+class LinkedList
+{
+public:
+    IntegerCell * top;
+    LinkedList(IntegerCell);
+    ~LinkedList();
+};
+
+LinkedList::LinkedList(IntegerCell topCell)
+{
+    top = new IntegerCell(topCell.value);
+}
+
+LinkedList::~LinkedList()
+{
+    free(top);
+}
+
+
+int main()
+{
+    IntegerCell *x = new IntegerCell(1);
+    cout << x << endl;
+    cout << "Hello World!" << endl;
+}
