d6cf686ff30666d2799c2f7e7b0beae2a32155be
fateful <vladislavvladislavovich@gmail.com>
2019-09-18 23:54:01 +0300

    Added binary search

--- Chapter 7 Search/search.cpp
+++ Chapter 7 Search/search.cpp
@@ -13,9 +13,34 @@ int linear_search(int * array, int size, int value)
     return -1;
 }
 
+int binary_search(int * array, int size, int value)
+{
+    int min_idx = 0;
+    int max_idx = size-1;
+    int mid_idx = (min_idx+max_idx)/2;
+
+    while(min_idx <= max_idx)
+    {
+        mid_idx = (min_idx+max_idx)/2;
+
+        if (value > array[mid_idx])
+        {
+            min_idx = mid_idx+1;
+        } else if (value < array[mid_idx]) {
+            max_idx = mid_idx-1;
+        } else {
+            return mid_idx;
+        }
+
+    }
+    return -1;
+}
+
 int main()
 {
-    int my_arr[5] = {3,4,1,4,5};
+    int my_arr[5] = {1, 2, 3, 4, 5};
 
-    cout << linear_search(my_arr, 5, 1);
+    cout << linear_search(my_arr, 5, 4) << endl;
+
+    cout << linear_search(my_arr, 5, 4) << endl;
 }
