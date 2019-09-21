084dca2ee56663b4fa8cd6322561a2db9b8ac970
fateful <vladislavvladislavovich@gmail.com>
2019-09-21 15:28:08 +0300

    Added Open Addressing (Linear)

--- Chapter 8 hash tables/hash_tables.cpp
+++ Chapter 8 hash tables/hash_tables.cpp
@@ -55,23 +55,86 @@ struct SeparateChaining
     vector<vector<int>> array;
 };
 
+// Открытая адресация с линейным пробированием
+struct LinearOpenAddressing
+{
+    LinearOpenAddressing()
+    {
+        array = vector<int>(HASH_SIZE);
+
+        for (size_t i = 0; i < HASH_SIZE; i++)
+        {
+            array[i] = -1;
+        }
+    }
+
+    void print()
+    {
+        for (size_t i = 0; i < HASH_SIZE; i++)
+        {
+            std::cout << array[i] << ", ";
+        }
+        std::cout << std::endl;
+    }
+
+    bool block_is_free(int block_num)
+    {
+        return array[block_num] == -1;
+    }
+
+    void append(int num)
+    {
+        int block_num = hash(num);
+        int try_number = 1;
+        while (block_num < HASH_SIZE && !block_is_free(block_num))
+        {
+            block_num += 1;
+            try_number += 1;
+        }
+        
+        if (block_num < HASH_SIZE)
+            array[block_num] = num;
+        else // should do resize
+            std::cout << "HASH TABLE IS FULL!" << std::endl;
+            
+    }
+
+    bool find(int num)
+    {
+        int block_num = hash(num);
+        int try_number = 1;
+        while (block_num < HASH_SIZE && !block_is_free(block_num))
+        {
+            if (array[block_num] == num)
+                return true;
+        }
+        return false;
+    }
+
+    vector<int> array;
+};
 
 int main()
 {
-    SeparateChaining hash_table;
+    LinearOpenAddressing hash_table;
     hash_table.print(); 
     std::cout << "-----------" << std::endl;
 
-    hash_table.append(10);
-    hash_table.append(111);
+    hash_table.append(61);
 
-    hash_table.append(28);
+    hash_table.append(22);
+
+    hash_table.append(34);
+    hash_table.append(84);
+    hash_table.append(14);
+
     hash_table.append(38);
+    hash_table.append(71);
 
     hash_table.print(); 
 
     
     std::cout << "-----------" << std::endl;
 
-    std::cout << hash_table.find(111);
+    std::cout << hash_table.find(72);
 }
