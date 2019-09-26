bc2cc318448dd7d7a58ccf8156b9938e7ffa20c4
fateful <vladislavvladislavovich@gmail.com>
2019-09-26 16:06:23 +0300

    Finished avl with insert and remove

--- Chapter 11 Advanced Trees/avl_tree.cpp
+++ Chapter 11 Advanced Trees/avl_tree.cpp
@@ -62,3 +62,82 @@ avl_node* rotate_left(avl_node* q)
     fix_height(p);
     return p;
 }
+
+avl_node* balance(avl_node* p)
+{
+    fix_height(p);
+    // if right side is heavier
+    if (balance_factor(p) == 2)
+    {
+        if (balance_factor(p->right) < 0)
+            p->right = rotate_right(p->right);
+        return rotate_left(p);
+    }
+
+    // if left side is heavier
+    if (balance_factor(p) == -2)
+    {
+        if (balance_factor(p->left) > 0)
+            p->left = rotate_left(p->left);
+        return rotate_right(p);
+    }
+
+    // no need for balancing
+    return p;
+}
+
+avl_node* insert(avl_node* p, int key)
+{
+    if (!p)
+        return new avl_node(key);
+
+    if (key > p->key)
+        p->left = insert(p->right, key);
+    else
+        p->right = insert(p->left, key);
+
+    return balance(p);
+}
+
+avl_node* find_min(avl_node* p)
+{
+    return p->left ? find_min(p->left) : p; 
+}
+
+avl_node* remove_min(avl_node* p)
+{
+    if (p->left == 0)
+        return p->right; // why??
+
+    p->left = remove_min(p->left);
+    return balance(p);
+}
+
+avl_node* remove(avl_node* p, int k)
+{
+    if (p == 0)
+        return 0;
+
+    if (k > p->key)
+        p->right = remove(p->right, k);
+    else if (k < p->key)
+        p->left = remove(p->right, k);
+    else
+    {
+        // we found the key!!
+        avl_node* q = p->left;
+        avl_node* r = p->right;
+        delete p;
+
+        // if theres only left child, we swap it with its father
+        if (r == 0)
+            return q;
+
+        avl_node* min_node = find_min(r);
+        min_node->right = remove_min(r);
+        min_node->left = q;
+
+        return balance(min_node);
+    }
+    return balance(p);
+}
